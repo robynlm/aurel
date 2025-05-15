8dd1529bdba4f9d6fa9cab7eef31cff566120e4a
Robyn L. Munoz <robyn.munoz@yahoo.fr>
2025-05-15 12:53:19 +0100

    fix how aurel is called

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.abspath('../src'))
 import aurel
 
 # Configuration file for the Sphinx documentation builder.
